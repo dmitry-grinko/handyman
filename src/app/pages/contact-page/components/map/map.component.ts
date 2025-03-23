74e3836851ce8b4802bab7fabb20fda6cb15c93c
Dmitry Grinko <dm.grinko@gmail.com>
2025-03-23 16:45:45 -0700

    test

--- src/app/pages/contact-page/components/map/map.component.ts
+++ src/app/pages/contact-page/components/map/map.component.ts
@@ -1,6 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { GoogleMap } from '@angular/google-maps';
-
 
 @Component({
   selector: 'app-map',
@@ -9,7 +8,31 @@ import { GoogleMap } from '@angular/google-maps';
   templateUrl: './map.component.html',
   styleUrl: './map.component.scss'
 })
-export class MapComponent {
+export class MapComponent implements OnInit {
   center: google.maps.LatLngLiteral = { lat: 45.5152, lng: -122.6784 }; // Portland
   zoom = 12;
+  mapOptions!: google.maps.MapOptions;
+
+  ngOnInit() {
+    // Define map options to customize appearance
+    this.mapOptions = {
+      // Remove UI controls
+      disableDefaultUI: true, // Removes all controls
+      // Or selectively disable specific controls:
+      // zoomControl: false,
+      // mapTypeControl: false,
+      // streetViewControl: false,
+      // fullscreenControl: false,
+      
+      // Change map style (optional)
+      styles: [
+        // You can add a custom style array here
+        // Example style to make the map grayscale:
+        {
+          featureType: 'all',
+          stylers: [{ saturation: -100 }]
+        }
+      ]
+    };
+  }
 }
